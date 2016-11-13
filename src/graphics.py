05851a1a1d43ebacbda74a5100e18be979933197
ArturoAguila <churro777@gmail.com>
2016-11-12 18:56:10 -0700

    double all images by using transform.scale and got rid of unnecesarry functions

--- src/graphics.py
+++ src/graphics.py
@@ -1,3 +1,4 @@
+
 import pygame
 
 images = {}
@@ -8,46 +9,34 @@ _screen = None
 _entities = []
 _map = None
 
+# adds entity to entities - which holds what needs to be drawn
+# each entity needs sprite_sheet, frame, weight
+# , and FRAMES (ann array of x and y coods representing the positions on teh sprite sheet)
+# also an x and y position
 def register(entity):
 	global _entities
 	if entity not in _entities:
 		_entities.append(entity)
-		
-def set_map(map):
-	global _map
-	_map = map
 
+# Creates graphics object. sets its width, height and title of the window of the game
 def init(width, height, title = 'Jogging Lawrence'):
 	global _width, _height, _screen
 	_width = width
 	_height = height
 	
 	pygame.display.init()
+	# creates window
 	_screen = pygame.display.set_mode((_width, _height), pygame.DOUBLEBUF)
+	# sets title of the window
 	pygame.display.set_caption(title)
 
-def draw_map():
-	global _screen, _map
-	for x in xrange(_map.width):
-		for y in xrange(_map.height):
-			tile = _map[x, y]
-			tilex = tile % 30
-			tiley = tile / 30
-	
-			_screen.blit(
-					_map.tileset.subsurface((tilex * 16, tiley * 16, 16, 16)),
-					(x * 16, y * 16)
-			)
 
 def update():
 	global _screen, _entities
 	_screen.fill((0, 0, 0))
-	draw_map()
 	for entity in _entities:
-		_screen.blit( entity.sprite_sheet.subsurface(
-								entity.FRAMES[12 * entity.weight + int(entity.frame)]
-						),
-						(entity.x, entity.y))
+		_screen.blit( entity.sprite_sheet.subsurface(entity.FRAMES[12 * entity.weight + int(entity.frame)])
+					,(entity.x, entity.y))	
 	pygame.display.flip()
 
 
@@ -57,6 +46,7 @@ def load_image(path):
 		return images[path]
 		
 	image = pygame.image.load(path)
+	image = pygame.transform.scale(image, (image.get_width() * 2, image.get_height() * 2))
 	image.convert()
 	images[path] = image
 	return image
