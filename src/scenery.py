72c645303532ffb4afd3ad9f3edd99158f96edb5
ArturoAguila <churro777@gmail.com>
2016-11-12 17:40:24 -0700

    created and stubbed out scenery.py

---
+++ src/scenery.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+background
+ground
+background_speed
+ground_speed
+
+def drawBackground():
+
+
+def drawGround():
+
+
+def draw():
+
+
+def getGroundSpeed():
+
+
+def playBackgroundMusic():
+
