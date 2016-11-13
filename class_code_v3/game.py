cd27ef7d83f064f918c128ab8a380464dcb52ce8
ArturoAguila <churro777@gmail.com>
2016-11-12 17:17:12 -0700

    added the class code. not the lastest but works great. character moves and there is some background. no hit detection though

---
+++ class_code_v3/game.py
@@ -0,0 +1,44 @@
+import pygame
+
+import map
+import events
+import objects
+import graphics
+
+framerate = 30
+ms_per_frame = (1. / framerate) / 1000
+
+graphics.init(240, 240)
+
+minotaur = objects.Minotaur(10, 10)
+minotaur.facing_right = True
+minotaur.action = objects.Action.WALK
+graphics.register(minotaur)
+
+print dir(map)
+game_map = map.sample_map()
+graphics.set_map(game_map)
+	
+clock = pygame.time.Clock()
+
+run = True
+lag = 0
+while(run):
+	ms = clock.tick(framerate)
+	lag = lag + ms - ms_per_frame
+	events.update()
+	minotaur.update()
+	
+	if lag > ms_per_frame:
+		graphics.update()
+		lag -= ms_per_frame
+	
+	for e in events.event_queue:
+		if e.type == pygame.QUIT:
+			run = False
+			
+		elif e.type == pygame.KEYUP:
+			if e.key ==  pygame.K_F4 and (e.mod & pygame.KMOD_ALT):
+				run = False
+
+pygame.quit()
