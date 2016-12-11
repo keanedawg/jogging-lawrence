5704478fb368892a4f1edea36ee13014fdd90695
Matt <mattsmith533811@yahoo.com>
2016-12-10 18:02:52 -0700

    get audio working, disabled end song because of loop

--- src/game.py
+++ src/game.py
@@ -13,10 +13,21 @@ import objects
 import score
 import audio
 
-<<<<<<< HEAD
-##start graphics
-=======
 run = True
+
+try:
+	pygame.mixer.pre_init(44100, -16, 2, 4096)
+	pygame.mixer.init()
+except:
+	print "You don't have audio support."
+   	con.audio_support = False
+
+if con.audio_support:
+	audio.load_audio()
+
+	pygame.mixer.music.load(os.path.join('audio','jl_music.ogg'))
+	pygame.mixer.music.set_volume(.9)
+	pygame.mixer.music.play(-1)
 
 def game_start():
 	global run,scene,lawrence,sc,ents,clock,lag,restart
@@ -72,20 +83,9 @@ def game_loop():
 					lawrence.collide(e)
 					restart = True
 					if con.audio_support:
-						pygame.mixer.stop
-	
-						effect = pygame.mixer.Sound(os.path.join('audio','jl_slap.ogg'))
-						effect.play()
-	
-						effect = pygame.mixer.Sound(os.path.join('audio','jl_slap.ogg'))
-						effect.play()
-	
 						pygame.mixer.music.stop()
-	
-						pygame.mixer.music.load(os.path.join('audio','endTest.ogg'))
-						pygame.mixer.music.set_volume(.5)
-						pygame.mixer.music.play(-1)
-	
+						#audio.end_song.play(-1)
+
 		if lag > con.ms_per_frame:
 			graphics.update()
 			lag -= con.ms_per_frame
@@ -107,99 +107,16 @@ def game_end():
 	gamespeed.reset()
 	sc.reset()
 
->>>>>>> 046f492412d10a2b51e0a6077f66bcb807cf7b21
 graphics.init(con.SCR_WIDTH, con.SCR_HEIGHT)
 ents = []
 sc = score.Score()
 
 ##start audio and check for audio support
-try:
-	pygame.mixer.pre_init(44100, -16, 2, 4096)
-	pygame.mixer.init()
-except:
-	print "You don't have audio support."
-   	con.audio_support = False
 
-if con.audio_support:
-	audio.load_audio()
-
-	pygame.mixer.music.load(os.path.join('audio','jl_music.ogg'))
-	pygame.mixer.music.set_volume(.9)
-	pygame.mixer.music.play(-1)
-
-<<<<<<< HEAD
-
-scene = scenery.Scenery()
-lawrence = person.Person()
-sc = score.Score()
-
-ents = []
-for x in xrange(1,1000):
-	if x % 16 == 1:
-		ents.append(objects.Bird(x * random.randint(50, 500)))
-	elif x % 16 == 5:
-		ents.append(objects.Ball(x * random.randint(50, 500)))
-	elif x % 16 == 9:
-		ents.append(objects.Cone(x * random.randint(50, 500)))
-	elif x % 16 == 13:
-		ents.append(objects.Hurdle(x * random.randint(50, 500)))
-
-graphics.register(scene)
-graphics.register(lawrence)
-graphics.register(sc)
-
-for e in ents:
-	graphics.register(e)
-
-clock = pygame.time.Clock()
-
-run = True
-lag = 0
-while(run):
-	ms = clock.tick(con.framerate)
-	lag = lag + ms - con.ms_per_frame
-
-	# Game Logic
-	if lawrence.isAlive():
-		gamespeed.update()
-
-	events.update()
-	lawrence.update()
-	scene.update()
-	for e in ents:
-		e.update()
-
-        # Update Score
-        if gamespeed.frame % 15 == 0:
-                sc.addScore(1)
-
-	# Collision
-	if lawrence.isAlive():
-		for e in ents:
-			if lawrence.rect.colliderect(e.rect):
-				lawrence.collide(e)
-				if con.audio_support:
-					audio.main_song.stop()
-					audio.end_song.play(-1)
-
-	if lag > con.ms_per_frame:
-		graphics.update()
-		lag -= con.ms_per_frame
-
-	for e in events.event_queue:
-		if e.type == pygame.QUIT:
-			run = False
-
-		elif e.type == pygame.KEYUP:
-			if e.key ==  pygame.K_F4 and (e.mod & pygame.KMOD_ALT):
-				run = False
-	
-=======
 while run:
 	game_start()
 	game_loop()
 	game_end()
 
->>>>>>> 046f492412d10a2b51e0a6077f66bcb807cf7b21
 pygame.quit()
 
